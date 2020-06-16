3aaf8d66e4cfd2901348e65663b8ee3b45ab7bdd
SH\shs-xxaqbzytl <shs-xxaqbzytl@chinaunicom.cn>
2020-06-16 17:31:09 +0800

    获取非必传参数

--- src/main/java/com/tanglei/springbootstudydemo/controller/HelloController.java
+++ src/main/java/com/tanglei/springbootstudydemo/controller/HelloController.java
@@ -1,13 +1,17 @@
 package com.tanglei.springbootstudydemo.controller;
 
 import com.tanglei.springbootstudydemo.global.myannotation.Check;
+import com.tanglei.springbootstudydemo.global.myaspect.ParamsAspect;
 import com.tanglei.springbootstudydemo.result.ResponseResult;
 import com.tanglei.springbootstudydemo.service.HelloService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Constants;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
@@ -16,7 +20,7 @@ import java.util.Map;
 @Controller
 @RequestMapping("/v1")
 public class HelloController {
-
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
     @Autowired
     public HelloService helloService;
 
@@ -34,7 +38,8 @@ public class HelloController {
     @RequestMapping(value="/users",method = RequestMethod.POST,produces ="application/json; charset=utf-8")
     @Check(params = {"size"})
     @ResponseBody
-    public ResponseResult getUsers(){
+    public ResponseResult getUsers(@RequestParam(value="name", required = false) String name){
+        logger.info("name:"+name);
         return  helloService.getUsers();
     }
 
